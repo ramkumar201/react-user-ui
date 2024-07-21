ea4baf6212ed9980157e0b7ff63688b77db078bb
G Ramkumar <ramkumar@teamenova.com>
2024-07-21 20:47:48 +0530

    commented code

--- src/helper/Validate.js
+++ src/helper/Validate.js
@@ -1,14 +1,3 @@
-import toast from "react-hot-toast";
-
-export async function emailValidate(values) {
-    let error = {};
-    if (!values.email) {
-        error.email = toast.error("Email Required...!")
-    } else if (values.email.includes(" ") || (!/\S+@\S+\.\S+/.test(values.email))) {
-        error.email = toast.error("Invalid Email...!")
-    }
-    return error;
-}
 
 export async function checkEmail(value) {
     let returnValue = false;
