af4db3c6b80107b90ed834dc33a58c4b54375771
G Ramkumar <ramkumar@teamenova.com>
2024-08-10 19:00:13 +0530

    error fixing

--- src/hook/useAuth.jsx
+++ src/hook/useAuth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useMemo } from "react";
+import { createContext, useContext, useMemo, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import { useLocalStorage } from "./useLocalStorage";
 
@@ -9,20 +9,20 @@ export const AuthProvider = ({ children }) => {
     const [user, setUser] = useLocalStorage("user", null);
     const navigate = useNavigate();
 
-    const logout = () => {
+    const logout = useCallback(() => {
         setUser(null);
         navigate('/login');
-    }
+    }, [setUser, navigate]);
 
-    const login = async (data) => {
+    const login = useCallback(async (data) => {
         setUser(data);
         console.log('user',  user);
         navigate('/', { replace: true })
-    }
+    }, [setUser, navigate, user]);
 
     const value = useMemo(() => ({
         user, login, logout
-    }), [user]);
+    }), [user, login, logout]);
 
     return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
 }
